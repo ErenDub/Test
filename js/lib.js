ac177e1418a8795781a68361b510697ce2aec44d
Roin Kuntchulia <foxmr88@gmail.com>
2021-03-10 15:22:29 +0400

    third commit

--- js/lib.js
+++ js/lib.js
@@ -1,13 +1,16 @@
 var menuBorder = document.getElementById ('mobile-menu');
+var menuBurger = document.getElementById ('burger');
 var k = 0;
 function mobileMenu (){
 	
 	if (k == 0) {
 		menuBorder.style.display = 'block';
+		menuBurger.style.opacity = '0';
 		k++;
 	}
 	else{
 		menuBorder.style.display = 'none';
+		menuBurger.style.opacity = '1';
 		k--;
 	}
 }
